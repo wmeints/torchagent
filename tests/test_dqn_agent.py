Fix issue with Q-Value calculation

tests/test_dqn_agent.py
@@ -39,18 +39,18 @@ def test_agent_predict(agent):
 
     assert result > -1
 
-def test_agent_train_double_q(double_agent):
-    for _ in range(0, 11):
+def test_agent_train_double_q(double_q_agent):
+    for _ in range(0, 64):
         state = torch.from_numpy(np.random.random((1,10)).astype(np.float32)).to(device)
         next_state = torch.from_numpy(np.random.random((1,10)).astype(np.float32)).to(device)
-        action = double_agent.act(state)
+        action = double_q_agent.act(state)
 
-        double_agent.record(state, action, next_state, 1, False)
+        double_q_agent.record(state, action, next_state, 1, False)
 
-    double_agent.train()
+    double_q_agent.train()
 
 def test_agent_train(agent):
-    for _ in range(0, 11):
+    for _ in range(0, 64):
         state = torch.from_numpy(np.random.random((1,10)).astype(np.float32)).to(device)
         next_state = torch.from_numpy(np.random.random((1,10)).astype(np.float32)).to(device)
         action = agent.act(state)

torchagent/agents/dqn.py
@@ -34,8 +34,8 @@ class DQNAgent:
             tau (float): Factor controlling the speed at which the target model is updated
         """
 
-        self.policy = policy if policy is not None else EpsilonGreedyPolicy()
-        self.test_policy = test_policy if test_policy is not None else GreedyPolicy()
+        self.policy = policy if policy is not None else EpsilonGreedyPolicy(num_actions)
+        self.test_policy = test_policy if test_policy is not None else GreedyPolicy(num_actions)
         self.memory = memory
         self.training = training
         self.batch_size = batch_size
@@ -106,32 +106,43 @@ class DQNAgent:
         reward_batch = torch.cat(batch.reward)
         next_state_batch = torch.cat(batch.next_state)
         terminated_batch = torch.tensor(
-            [1. if s == False else 0. for s in batch], device=device, dtype=torch.float)
+            [1. if s == False else 0. for s in batch.done], device=device, dtype=torch.float)
 
-        # Note: We're using a second policy network to stabalize the learning process.
+        # We're trying to minimize a loss that is defined as follows:
+        # loss = sum(sqr(reward + gamma * target_Q(next_state) - Q(state)))
+        #
+        # Please note that you can use the huber loss also, but it uses the same inputs:
+        # - Q(next_state, a) --> Q-values for the target state
+        # - Q(state, a) --> Q-values for the current state
+        #
+        # We're using a second network to calculate the Q-values for the target state.
+        # this stabalizes the training process so it becomes more predictable.
+        
         if not self.enable_dqn:
-            # In q learning we estimate the values of the actions that we previously took.
             target_q_values = self.target_model(next_state_batch)
-            q_batch = target_q_values.gather(1, action_batch)
+            target_q_values = target_q_values.gather(1, action_batch)
+            q_values = self.model(state_batch)
+            q_values = q_values.gather(1, action_batch)
         else:
             # In double-q learning we use the policy network to predict the actions instead of using the actually performed actions.
             # We then use these predicted actions to calculate the expected value of these actions.
-            q_values = self.model(next_state_batch)
+            q_values = self.target_model(state_batch)
             estimated_actions = q_values.max(1)[1]
 
             target_q_values = self.target_model(next_state_batch)
-            q_batch = target_q_values.gather(1, estimated_actions)
+            target_q_values = target_q_values.gather(1, estimated_actions)
 
         # Calculate the discounted award for the actions taken by the agent.
         # Then reset the reward for the actions that caused the episode to end.
-        discounted_reward = self.gamma * q_batch
+        discounted_reward = self.gamma * target_q_values
         discounted_reward = terminated_batch * discounted_reward
 
         # Calculate the expected reward using the discounted reward and the actual reward.
         targets = discounted_reward + reward_batch
+        targets = targets.detach()
 
         # Now calculate the loss for the policy that we're currently using.
-        loss_value = self.loss(q_batch, targets.unsqueeze(1))
+        loss_value = self.loss(q_values, targets.unsqueeze(1))
 
         # Finally, optimize the policy using the choosen optimizer.
         self.optimizer.zero_grad()

torchagent/policy.py
@@ -2,6 +2,9 @@ import numpy as np
 import torch
 import random
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
 class Policy:
     """ 
     Inherit from this class to implement a new policy.
@@ -19,15 +22,18 @@ class Policy:
         """
         raise NotImplementedError()
 
+
 class GreedyPolicy(Policy):
     """
     The greedy policy doesn't use exploration at all, instead is chooses the best action based on the provided action-values.
     """
-    def __init__(self):
+
+    def __init__(self, num_actions):
         """
         Initializes a new instance of the greedy policy.
         """
         super(GreedyPolicy, self).__init__()
+        self.num_actions = num_actions
 
     def select_action(self, q_values):
         """
@@ -36,8 +42,8 @@ class GreedyPolicy(Policy):
         if len(q_values.size()) > 1:
             raise AssertionError("q_values has more than one dimension")
 
-        action_tensor = q_values.max(0)[1]
-        action_tensor = action_tensor.view(1,1)
+        action_tensor = q_values.max(len(q_values.size())-1)[1]
+        action_tensor = action_tensor.view(1, 1)
 
         return action_tensor
 
@@ -49,7 +55,7 @@ class EpsilonGreedyPolicy(Policy):
     while a low value means that we're going to stick to what works and exploit the existing paths more.
     """
 
-    def __init__(self, epsilon=0.1):
+    def __init__(self, num_actions, epsilon=0.1):
         """
         Initializes the epsilon-greedy policy
 
@@ -58,6 +64,7 @@ class EpsilonGreedyPolicy(Policy):
         """
         super(EpsilonGreedyPolicy, self).__init__()
         self.epsilon = epsilon
+        self.num_actions = num_actions
 
     def select_action(self, q_values):
         """
@@ -70,15 +77,12 @@ class EpsilonGreedyPolicy(Policy):
             The index of the selected action
         """
 
-        trailing_dim = len(q_values.size()) - 1
-
-        if np.random.uniform() < self.epsilon:    
-            num_actions = q_values.size(trailing_dim)
+        if np.random.uniform() < self.epsilon:
             selected_action = torch.tensor(
-                [[random.randrange(num_actions)]],
-                dtype=torch.long)
+                [[random.randrange(self.num_actions)]],
+                dtype=torch.long, device=device)
         else:
-            selected_action = q_values.max(trailing_dim)[1]
-            selected_action = selected_action.view(1,1)
+            selected_action = q_values.max(len(q_values.size())-1)[1]
+            selected_action = selected_action.view(1, 1)
 
         return selected_action